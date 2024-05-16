56f68d66752ce4238375ee6f2edf64537d48b0b0
Maykol Castro <maycolcastroesp19@gmail.com>
2024-05-16 05:44:25 -0600

    new app

---
+++ resources/views/tenants/create.blade.php
@@ -0,0 +1,77 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Agregar Tenants') }}
+           
+
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <form method="POST" action="{{ route('tenants.store') }}">
+                        @csrf
+                
+                        <!-- Name -->
+                        <div>
+                            <x-input-label for="domain" :value="__('Nombre')" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+                
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-input-label for="email" :value="__('Correo')" />
+                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <div class="mt-4">
+                            <x-input-label for="domain_name" :value="__('Nombre del Dominio')" />
+                            <x-text-input id="domain_name" class="block mt-1 w-full" type="text" name="domain_name" :value="old('domain_name')" required autofocus autocomplete="domain_name" />
+                            <x-input-error :messages="$errors->get('domain_name')" class="mt-2" />
+                        </div>
+                
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <x-input-label for="password" :value="__('Contraseña')" />
+                
+                            <x-text-input id="password" class="block mt-1 w-full"
+                                            type="password"
+                                            name="password"
+                                            required autocomplete="new-password" />
+                
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+                
+                        <!-- Confirm Password -->
+                        <div class="mt-4">
+                            <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
+                
+                            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                            type="password"
+                                            name="password_confirmation" required autocomplete="new-password" />
+                
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                        </div>
+                
+                        <div class="flex items-center justify-end mt-4">
+                            
+                
+                            <x-primary-button class="ml-4">
+                                {{ __('Crear') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+
+
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/tenants/index.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tenants') }}
+            <x-btn-link class="ml-4 float-right" href="{{ route('tenants.create') }}">Agregar Tenant</x-btn-link>
+
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{-- inicio --}}
+                    <div class="py-12">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div class="p-6 text-gray-900">
+
+                                    <div class="relative overflow-x-auto">
+                                        <table class="w-full text-sm text-left text-gray-700">
+                                            <thead
+                                                class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                                <tr>
+                                                    <th scope="col" class="px-6 py-3">Nombre</th>
+                                                    <th scope="col" class="px-6 py-3">Correo</th>
+                                                    <th scope="col" class="px-6 py-3">Dominio</th>
+                                                    <th scope="col" class="px-6 py-3">Estado</th>
+                                                </tr>
+
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($tenants as $tenant)
+                                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                        <th scope="row"
+                                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                            {{ $tenant->name }}
+                                                        </th>
+                                                        <td class="px-6 py-4">{{ $tenant->email }}</td>
+                                                        <td class="px-6 py-4">
+                                                            @foreach ($tenant->domains as $domain)
+                                                                {{ $domain->domain }}{{ $loop->last ? '' : ',' }}
+                                                            @endforeach
+                                                        </td>
+                                                        <td class="px-6 py-4"></td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    {{-- fin --}}
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
